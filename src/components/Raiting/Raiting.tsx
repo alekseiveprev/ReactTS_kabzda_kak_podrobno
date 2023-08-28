0d6bc53b31d2ca4880bae9ec69b7af99e8d48848
alekseiveprev <alekseiveprev@gmail.com>
2023-08-28 23:16:35 +0300

    init: start project

---
+++ src/components/Raiting/Raiting.tsx
@@ -0,0 +1,114 @@
+import React from "react";
+
+type RaitingPropsType = {
+    value: 0 | 1 | 2 | 3 | 4 | 5
+}
+
+export function Raiting(props: RaitingPropsType) {
+    console.log("Raiting rendering");
+
+    return (
+        <div>
+            <Star selected={props.value > 0}/>
+            <Star selected={props.value > 1}/>
+            <Star selected={props.value > 2}/>
+            <Star selected={props.value > 3}/>
+            <Star selected={props.value > 4}/>
+        </div>
+    )
+
+    /*if (props.value === 0) {
+        return (
+            <div>
+                <Star selected={false}/>
+                <Star selected={false}/>
+                <Star selected={false}/>
+                <Star selected={false}/>
+                <Star selected={false}/>
+            </div>
+        )
+    }
+    if (props.value === 1) {
+        return (
+            <div>
+                <Star selected={true}/>
+                <Star selected={false}/>
+                <Star selected={false}/>
+                <Star selected={false}/>
+                <Star selected={false}/>
+            </div>
+        )
+    }
+    if (props.value === 2) {
+        return (
+            <div>
+                <Star selected={true}/>
+                <Star selected={true}/>
+                <Star selected={false}/>
+                <Star selected={false}/>
+                <Star selected={false}/>
+            </div>
+        )
+    }
+    if (props.value === 3) {
+        return (
+            <div>
+                <Star selected={true}/>
+                <Star selected={true}/>
+                <Star selected={true}/>
+                <Star selected={false}/>
+                <Star selected={false}/>
+            </div>
+        )
+    }
+    if (props.value === 4) {
+        return (
+            <div>
+                <Star selected={true}/>
+                <Star selected={true}/>
+                <Star selected={true}/>
+                <Star selected={true}/>
+                <Star selected={false}/>
+            </div>
+        )
+    }
+    if (props.value === 5) {
+        return (
+            <div>
+                <Star selected={true}/>
+                <Star selected={true}/>
+                <Star selected={true}/>
+                <Star selected={true}/>
+                <Star selected={true}/>
+            </div>
+        )
+    }
+    return (
+        <div>
+            <Star selected={false}/>
+            <Star selected={false}/>
+            <Star selected={false}/>
+            <Star selected={false}/>
+            <Star selected={false}/>
+        </div>
+    )*/
+
+
+}
+
+type StarPropsType = {
+    selected: boolean
+}
+
+function Star(props: StarPropsType) {
+    //debugger
+    console.log("Star rendering");
+    /*{ props.selected === true && return (<span><b>star</b> </span>) }   // какаято хрень у меня*/
+
+
+    if (props.selected === true) {
+        return (<span><b>star</b> </span>)
+    } else {
+        return (<span>star </span>)
+    }
+}
