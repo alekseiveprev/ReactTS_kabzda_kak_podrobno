d004732dde0648d684c2f0440e4714a9ae07b135
alekseiveprev <72622572+alekseiveprev@users.noreply.github.com>
2023-08-30 14:29:58 +0300

    init: start 06-task

--- src/App.tsx
+++ src/App.tsx
@@ -1,41 +1,36 @@
 import React from 'react';
 import './App.css';
-import Accordion from "./components/Accordion/Accordion";
-import {Raiting} from "./components/Raiting/Raiting";
+import Accordion from './components/Accordion/Accordion';
+import { Raiting } from './components/Raiting/Raiting';
 
 function App() {
-    return (
-        <div>
-            <PageTitle title={'This is App component'}/>
-            <PageTitle title={'My friends'}/>
-            Article 1
-            <Raiting value={3}/>
-            <Accordion titleValue={'Menu'} collapsed={true}/>
-            <Accordion titleValue={'Users'} collapsed={false}/>
-            Article 2
-            <Raiting value={0}/>
-            <Raiting value={1}/>
-            <Raiting value={2}/>
-            <Raiting value={3}/>
-            <Raiting value={4}/>
-            <Raiting value={5}/>
-        </div>
-    );
+  return (
+    <div>
+      <PageTitle title={'This is App component'} />
+      <PageTitle title={'My friends'} />
+      Article 1
+      <Raiting value={3} />
+      <Accordion titleValue={'Menu'} collapsed={true} />
+      <Accordion titleValue={'Users'}  collapsed={false}/>
+      Article 2
+      <Raiting value={0} />
+      <Raiting value={1} />
+      <Raiting value={2} />
+      <Raiting value={3} />
+      <Raiting value={4} />
+      <Raiting value={5} />
+    </div>
+  );
 }
 
 type PageTitlePropsType = {
-    title: string
-}
+  title: string;
+};
 
 function PageTitle(props: PageTitlePropsType) {
-    // debugger
-    console.log("PageTitle rendering")
-    return (
-        <h1>
-            {props.title}
-        </h1>
-    )
+  // debugger
+  console.log('PageTitle rendering');
+  return <h1>{props.title}</h1>;
 }
 
 export default App;
-
